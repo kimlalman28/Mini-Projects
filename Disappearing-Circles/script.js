d069bae93a6c53c44f195128ed6c3cc79eaa3e32
Kimberly Lalmansingh <kimlalman28@gmail.com>
2018-01-17 16:24:56 -0500

    added js

---
+++ Disappearing-Circles/script.js
@@ -0,0 +1,11 @@
+document.getElementById("red").onclick = function() {
+    document.getElementById("red").style.display = "none";
+}
+
+document.getElementById("blue").onclick = function() {
+    document.getElementById("blue").style.display = "none";
+}
+
+document.getElementById("green").onclick = function() {
+    document.getElementById("green").style.display = "none";
+}
